28a0b2d41396c086ddab3ccba0b38c2a3a949d3c
sirselim <miles.benton84@gmail.com>
2019-06-26 13:14:24 +1200

    added note to access complete survey data

--- dc_r_ggplot_code.R
+++ dc_r_ggplot_code.R
@@ -5,8 +5,249 @@
 
 # install required packages
 install.packages("tidyverse")
+install.packages("hexbin")
+install.packages("gridExtra")
+install.packages("cowplot")
+install.packages("viridis")
 
 # load packages
 library(tidyverse)
+library(hexbin)
+library(gridExtra)
+library(cowplot)
+library(viridis)
+
+
+
 
 surveys_complete <- read_csv("data_output/surveys_complete.csv")
+
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length))
+
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point()
+
+# Assign plot to a variable
+surveys_plot <- ggplot(data = surveys_complete, 
+                       mapping = aes(x = weight, y = hindfoot_length))
+
+# Draw the plot
+surveys_plot + 
+  geom_point()
+
+# This is the correct syntax for adding layers
+surveys_plot +
+  geom_point()
+
+# This will not add the new layer and will return an error message
+surveys_plot
++ geom_point()
+
+# using hexbin
+surveys_plot +
+  geom_hex()
+
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point()
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point(alpha = 0.1)
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point(alpha = 0.1, color = "blue")
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point(alpha = 0.1, aes(color = species_id))
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length, color = species_id)) +
+  geom_point(alpha = 0.1)
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length, color = species_id)) +
+  geom_jitter(alpha = 0.1)
+
+
+
+
+# challenge
+ggplot(data = surveys_complete, mapping = aes(x = species_id, y = weight)) +
+  geom_point(aes(color = plot_type))
+
+
+
+ggplot(data = surveys_complete, mapping = aes(x = species_id, y = weight)) +
+  geom_boxplot()
+
+
+ggplot(data = surveys_complete, mapping = aes(x = species_id, y = weight)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.3, color = "tomato")
+
+
+yearly_counts <- surveys_complete %>%
+  count(year, species_id)
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n)) +
+  geom_line()
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n, group = species_id)) +
+  geom_line()
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n, color = species_id)) +
+  geom_line()
+
+ggplot(data = yearly_counts, mapping = aes(x = year, y = n)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+yearly_sex_counts <- surveys_complete %>%
+  count(year, species_id, sex)
+
+ggplot(data = yearly_sex_counts, mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+
+ggplot(data = yearly_sex_counts, mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw() +
+  theme(panel.grid = element_blank())
+
+
+
+
+# challenge
+
+yearly_weight <- surveys_complete %>%
+  group_by(year, species_id) %>%
+  summarize(avg_weight = mean(weight))
+ggplot(data = yearly_weight, mapping = aes(x=year, y=avg_weight)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw()
+
+
+
+
+
+# One column, facet by rows
+yearly_sex_weight <- surveys_complete %>%
+  group_by(year, sex, species_id) %>%
+  summarize(avg_weight = mean(weight))
+ggplot(data = yearly_sex_weight, 
+       mapping = aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  facet_grid(sex ~ .)
+
+# One row, facet by column
+ggplot(data = yearly_sex_weight, 
+       mapping = aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  facet_grid(. ~ sex)
+
+
+ggplot(data = yearly_sex_counts, mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  labs(title = "Observed species in time",
+       x = "Year of observation",
+       y = "Number of individuals") +
+  theme_bw()
+
+
+ggplot(data = yearly_sex_counts, mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  labs(title = "Observed species in time",
+       x = "Year of observation",
+       y = "Number of individuals") +
+  theme_bw() +
+  theme(text=element_text(size = 16))
+
+
+ggplot(data = yearly_sex_counts, mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  labs(title = "Observed species in time",
+       x = "Year of observation",
+       y = "Number of individuals") +
+  theme_bw() +
+  theme(axis.text.x = element_text(colour = "grey20", size = 12, angle = 90, hjust = 0.5, vjust = 0.5),
+        axis.text.y = element_text(colour = "grey20", size = 12),
+        text = element_text(size = 16))
+
+
+
+grey_theme <- theme(axis.text.x = element_text(colour = "grey20", size = 12, angle = 90, hjust = 0.5, vjust = 0.5),
+                    axis.text.y = element_text(colour = "grey20", size = 12),
+                    text = element_text(size = 16))
+ggplot(surveys_complete, aes(x = species_id, y = hindfoot_length)) +
+  geom_boxplot() +
+  grey_theme
+
+
+
+
+
+
+# challenge
+
+
+
+
+
+
+
+
+spp_weight_boxplot <- ggplot(data = surveys_complete, 
+                             mapping = aes(x = species_id, y = weight)) +
+  geom_boxplot() +
+  xlab("Species") + ylab("Weight (g)") +
+  scale_y_log10()
+
+spp_count_plot <- ggplot(data = yearly_counts, 
+                         mapping = aes(x = year, y = n, color = species_id)) +
+  geom_line() + 
+  xlab("Year") + ylab("Abundance")
+
+grid.arrange(spp_weight_boxplot, spp_count_plot, ncol = 2, widths = c(4, 6))
+
+
+my_plot <- ggplot(data = yearly_sex_counts, 
+                  mapping = aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  labs(title = "Observed species in time",
+       x = "Year of observation",
+       y = "Number of individuals") +
+  theme_bw() +
+  theme(axis.text.x = element_text(colour = "grey20", size = 12, angle = 90, hjust = 0.5, vjust = 0.5),
+        axis.text.y = element_text(colour = "grey20", size = 12),
+        text=element_text(size = 16))
+ggsave("fig_output/yearly_sex_counts.png", my_plot, width = 15, height = 10)
+
+# This also works for grid.arrange() plots
+combo_plot <- grid.arrange(spp_weight_boxplot, spp_count_plot, ncol = 2, widths = c(4, 6))
+ggsave("fig_output/combo_plot_abun_weight.png", combo_plot, width = 10, dpi = 300)
+
+
+
+# cowplot
+
+plot.mpg <- ggplot(mpg, aes(x = cty, y = hwy, colour = factor(cyl))) + 
+  geom_point(size=2.5)
+
+plot.mpg
+
+plot.diamonds <- ggplot(diamonds, aes(clarity, fill = cut)) + geom_bar() +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5))
+
+plot.diamonds
+
+plot_grid(plot.mpg, plot.diamonds, labels = c("A", "B"))
+
+
+
